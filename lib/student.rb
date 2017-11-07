614786715a3ca4531e72f5b82258f261e911b977
github username <github email address>
2017-11-07 15:07:55 -0500

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,54 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  attr_accessor :name, :grade, :id
 
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
+    sql = "CREATE TABLE students(id INTEGER PRIMARY KEY, name TEXT, grade TEXT)"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO students (name, grade) VALUES (?,?)"
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    new_stud = Student.new(name, grade)
+    new_stud.save
+  end
+
+  def self.new_from_db(row)
+    name = row[1]
+    grade = row[2]
+    id =row[0]
+    new_stud = Student.new(name,grade,id)
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id =?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = '#{name}'"
+    row = DB[:conn].execute(sql).flatten
+    Student.new_from_db(row)
+  end
 end
